a3508c99704e50880a911fee746c40d6b50b8716
morrowpappitson <60286396+tiaraalviana878@users.noreply.github.com>
2026-06-10 21:44:51 +0700

    add more movies to seeder

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -32,5 +32,36 @@ class MovieSeeder extends Seeder
             'duration' => 148,
             'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/00/Spider-Man_No_Way_Home_poster.jpg'
         ]);
+        Movie::create([
+        'title' => 'Top Gun: Maverick',
+        'description' => 'Seorang pilot veteran kembali melatih generasi baru pilot tempur.',
+        'genre' => 'Action, Drama',
+        'duration' => 131,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/1/13/Top_Gun_Maverick_Poster.jpg'
+    ]);
+
+    Movie::create([
+        'title' => 'How to Train Your Dragon',
+        'description' => 'Hiccup menjalin persahabatan dengan naga Toothless.',
+        'genre' => 'Animation, Adventure, Fantasy',
+        'duration' => 98,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/9/99/How_to_Train_Your_Dragon_Poster.jpg'
+    ]);
+
+    Movie::create([
+        'title' => 'Dune',
+        'description' => 'Paul Atreides menghadapi konflik di planet Arrakis.',
+        'genre' => 'Sci-Fi, Adventure',
+        'duration' => 155,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg'
+    ]);
+
+    Movie::create([
+        'title' => 'Moana',
+        'description' => 'Seorang gadis pemberani berlayar untuk menyelamatkan rakyatnya.',
+        'genre' => 'Animation, Adventure',
+        'duration' => 107,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/2/26/Moana_Teaser_Poster.jpg'
+    ]);
+        }
     }
-}
